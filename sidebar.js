0f8de098b32b6234c86f18de1d1784e38d70d095
LauncherX Labs <timbohlsen0@gmail.com>
2025-12-09 02:57:32 +0100

    Created sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+// Toggle Sidebar for mobile or small screens
+document.addEventListener("DOMContentLoaded", () => {
+    const sidebar = document.getElementById("sidebar");
+    const toggleBtn = document.getElementById("toggleSidebar");
+
+    toggleBtn.addEventListener("click", () => {
+        sidebar.classList.toggle("collapsed");
+    });
+});
